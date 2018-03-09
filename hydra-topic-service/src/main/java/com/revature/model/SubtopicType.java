41db132f955f0bfc271844f271db20bc6c4bd3fc
mnnnnn <mnguyen5081@gmail.com>
2018-03-09 10:06:09 -0500

    implemented subtopic and topic models

---
+++ hydra-topic-service/src/main/java/com/revature/model/SubtopicType.java
@@ -0,0 +1,64 @@
+package com.revature.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table(name = "Subtopic_Type")
+public class SubtopicType {
+
+	@Id
+	@Column(name = "Type_ID")
+	@SequenceGenerator(name = "Subtopic_Type_ID_SEQ", sequenceName = "Subtopic_Type_ID_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Subtopic_Type_ID_SEQ")
+	private Integer id;
+
+	@Column(name = "Type_Name")
+	private String name;
+
+	public SubtopicType() {
+		//Empty Because No Args
+	}
+
+	public SubtopicType(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public SubtopicType(String name) {
+		super();//NOSONAR
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}//NOSONAR
+
+	public void setId(Integer id) {
+		this.id = id;
+	}//NOSONAR
+
+	public String getName() {
+		return name;
+	}//NOSONAR
+
+	public void setName(String name) {
+		this.name = name;
+	}//NOSONAR
+
+	@Override
+	public String toString() {
+		return "SubtopicType [id=" + id + ", name=" + name + "]";//NOSONAR
+	}
+
+}
---
+++ hydra-topic-service/src/main/java/com/revature/model/TopicName.java
@@ -0,0 +1,63 @@
+package com.revature.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table(name = "Topic_Name")
+public class TopicName {
+
+	@Id
+	@Column(name = "Topic_ID")
+	@SequenceGenerator(name = "TOPIC_NAME_ID_SEQ", sequenceName = "TOPIC_NAME_ID_SEQ", allocationSize=1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TOPIC_NAME_ID_SEQ")
+	private Integer id;
+
+	@Column(name = "Topic_Name")
+	private String name;
+
+	public TopicName() {
+		super();
+	}
+
+	public TopicName(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public TopicName(String name) {
+		super(); //NOSONAR
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}//NOSONAR
+
+	public void setId(Integer id) {
+		this.id = id;
+	}//NOSONAR
+
+	public String getName() {
+		return name;
+	}//NOSONAR
+
+	public void setName(String name) {
+		this.name = name;
+	}//NOSONAR
+
+	@Override
+	public String toString() {
+		return "TopicName [id=" + id + ", name=" + name + "]";//NOSONAR
+	}
+
+}
