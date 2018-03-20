e1ee4bc14cebe355c5a3d19658a53996e6e3e274
Mary Nguyen <ng.mary.m@gmail.com>
2018-03-20 14:46:23 -0400

    got rid of merge comments

--- hydra-topic-service/src/main/java/com/revature/repository/SubtopicRepository.java
+++ hydra-topic-service/src/main/java/com/revature/repository/SubtopicRepository.java
@@ -8,17 +8,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.revature.model.Subtopic;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 import com.revature.model.SubtopicType;
 
->>>>>>> a92bc7159a2b873a2b5b71092f38d285738dfd85
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-=======
+
 import com.revature.model.SubtopicName;
 import com.revature.model.SubtopicStatus;
->>>>>>> c26a9f9eee82c34e5cd923ec6d69a49be61c11c0
+
 
 
 @RepositoryRestResource
